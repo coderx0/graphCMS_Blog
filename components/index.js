ace243c8fd2f48bbb93a7eb7fa6eb44e364a3929
sumeet debnath <sumeetdeb100@gmail.com>
2022-01-22 11:48:43 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+export { default as Author } from "./Author";
+export { default as Comments } from "./Comments";
+export { default as CommentsForm } from "./CommentsForm";
+export { default as PostDetail } from "./PostDetail";
+export { default as PostWidget } from "./PostWidget";
+export { default as Categories } from "./Categories";
+export { default as Loader } from "./Loader";
+export { default as PostCard } from "./PostCard";
